f734f0d0d79f7da72b048baeb6c8f2db5ace8faf
CodeMat <codemat.biz@gmail.com>
2023-06-18 09:55:33 +0100

    updated

--- app/repay/page.js
+++ app/repay/page.js
@@ -1,9 +1,9 @@
-import React from 'react'
-
 const Page = () => {
   return (
-    <div>Page</div>
-  )
-}
+    <div className='px-4 py-12 '>
+      <h3 className='text-center text-lg'>Coming Soon...</h3>
+    </div>
+  );
+};
 
-export default Page
+export default Page;
--- components/MobileMenu.js
+++ components/MobileMenu.js
@@ -17,46 +17,45 @@ const MobileMenu = ({ profile, session }) => {
 
   const signOut = () => {
     supabase.auth.signOut();
-    router.refresh()
+    router.refresh();
   };
 
   return (
-    <Menu as='div' className='relative inline-block text-left sm:hidden'>
-      <div>
-        <Menu.Button className='p-1 rounded-lg bg-[#086daf] hover:bg-blue-900 text-[#ec6d25]'>
-          <HiMenuAlt3 className='text-4xl' aria-hidden='true' />
-        </Menu.Button>
-      </div>
+    <>
+      {session &&
+        <Menu as='div' className='relative inline-block text-left sm:hidden'>
+          <div>
+            <Menu.Button className='p-1 rounded-lg bg-[#086daf] hover:bg-blue-900 text-[#ec6d25]'>
+              <HiMenuAlt3 className='text-4xl' aria-hidden='true' />
+            </Menu.Button>
+          </div>
 
-      <Transition
-        as={Fragment}
-        enter='transform transition duration-100 ease-in'
-        enterFrom='transform scale-95 opacity-0'
-        enterTo='transform scale-100 opacity-100'
-        leave='transform transition duration-75 ease-out'
-        leaveFrom='transform scale-100 opacity-100'
-        leaveTo='transform scale-95 opacity-0'>
-        <Menu.Items
-          className='origin-top-right absolute bg-blue-50 right-0 flex flex-col mt-2 rounded-lg w-56 overflow-hidden shadow-2xl ring-1 ring-black ring-opacity-5 focus:ontline-none'
-          static>
-          <Menu.Item>
-            {({ active }) => (
-              <a
-                className={`${
-                  active ? "bg-blue-200 text-white" : "text-[#ec6d25] "
-                } font-semibold inline-flex items-center px-4 gap-x-3 text-2xl py-4 `}
-                href='/'>
-                <BiHomeSmile
-                  className={` ${
-                    active ? "text-blue-950" : "text-blue-300"
-                  } text-2xl font-semibold `}
-                />
-                HOME
-              </a>
-            )}
-          </Menu.Item>
-          {session && (
-            <>
+          <Transition
+            as={Fragment}
+            enter='transform transition duration-100 ease-in'
+            enterFrom='transform scale-95 opacity-0'
+            enterTo='transform scale-100 opacity-100'
+            leave='transform transition duration-75 ease-out'
+            leaveFrom='transform scale-100 opacity-100'
+            leaveTo='transform scale-95 opacity-0'>
+            <Menu.Items
+              className='origin-top-right absolute bg-blue-50 right-0 flex flex-col mt-2 rounded-lg w-56 overflow-hidden shadow-2xl ring-1 ring-black ring-opacity-5 focus:ontline-none'
+              static>
+              <Menu.Item>
+                {({ active }) => (
+                  <a
+                    className={`${active ? "bg-blue-200 text-white" : "text-[#ec6d25] "
+                      } font-semibold inline-flex items-center px-4 gap-x-3 text-2xl py-4 `}
+                    href='/'>
+                    <BiHomeSmile
+                      className={` ${active ? "text-blue-950" : "text-blue-300"
+                        } text-2xl font-semibold `}
+                    />
+                    HOME
+                  </a>
+                )}
+              </Menu.Item>
+
               {profile &&
                 profile.map((item) => (
                   <div key='item.id'>
@@ -64,16 +63,14 @@ const MobileMenu = ({ profile, session }) => {
                       <Menu.Item className='w-full'>
                         {({ active }) => (
                           <a
-                            className={`${
-                              active
+                            className={`${active
                                 ? "bg-blue-200 text-white"
                                 : "text-[#ec6d25] "
-                            } font-semibold inline-flex items-center px-4 gap-x-3 text-2xl py-4 `}
+                              } font-semibold inline-flex items-center px-4 gap-x-3 text-2xl py-4 `}
                             href='/admin'>
                             <RiAdminLine
-                              className={` ${
-                                active ? "text-blue-950" : "text-blue-300"
-                              } text-2xl font-semibold `}
+                              className={` ${active ? "text-blue-950" : "text-blue-300"
+                                } text-2xl font-semibold `}
                             />
                             ADMIN
                           </a>
@@ -82,76 +79,68 @@ const MobileMenu = ({ profile, session }) => {
                     )}
                   </div>
                 ))}
-            </>
-          )}
 
-          <Menu.Item>
-            {({ active }) => (
-              <a
-                className={`${
-                  active ? "bg-blue-200 text-white" : "text-[#ec6d25] "
-                } font-semibold inline-flex items-center px-4 gap-x-3 text-2xl py-4 `}
-                href='/repay'>
-                <GiPayMoney
-                  className={` ${
-                    active ? "text-blue-950" : "text-blue-300"
-                  } text-2xl font-semibold `}
-                />
-                REPAY
-              </a>
-            )}
-          </Menu.Item>
+              <Menu.Item>
+                {({ active }) => (
+                  <a
+                    className={`${active ? "bg-blue-200 text-white" : "text-[#ec6d25] "
+                      } font-semibold inline-flex items-center px-4 gap-x-3 text-2xl py-4 `}
+                    href='/repay'>
+                    <GiPayMoney
+                      className={` ${active ? "text-blue-950" : "text-blue-300"
+                        } text-2xl font-semibold `}
+                    />
+                    REPAY
+                  </a>
+                )}
+              </Menu.Item>
 
-          {session && (
-            <Menu.Item>
-              {({ active }) => (
-                <button
-                  onClick={signOut}
-                  className={`${
-                    active ? "bg-blue-200 text-red-500" : "text-[#ec6d25] "
-                  } font-semibold inline-flex items-center px-4 gap-x-3 text-2xl py-4 `}>
-                  <AiOutlineLogout
-                    className={` ${
-                      active ? "text-red-500" : "text-blue-300"
-                    } text-2xl font-semibold `}
-                  />
-                  SIGN OUT
-                </button>
-              )}
-            </Menu.Item>
-          )}
-          <div className='flex justify-center divide-x divide-gray-400 bg-blue-100'>
-            <Menu.Item className='flex justify-center'>
-              {({ active }) => (
-                <a
-                  className={`${
-                    active ? "bg-blue-200" : "text-white"
-                  } w-full font-semibold inline-flex items-center justify-center gap-x-3 text-amber-900 text-center py-4 `}
-                  href='#'>
-                  <MdCall
-                    className={`  text-blue-950 text-2xl font-semibold `}
-                  />
-                </a>
-              )}
-            </Menu.Item>
+              <Menu.Item>
+                {({ active }) => (
+                  <button
+                    onClick={signOut}
+                    className={`${active ? "bg-blue-200 text-red-500" : "text-[#ec6d25] "
+                      } font-semibold inline-flex items-center px-4 gap-x-3 text-2xl py-4 `}>
+                    <AiOutlineLogout
+                      className={` ${active ? "text-red-500" : "text-blue-300"
+                        } text-2xl font-semibold `}
+                    />
+                    SIGN OUT
+                  </button>
+                )}
+              </Menu.Item>
 
-            <Menu.Item>
-              {({ active }) => (
-                <a
-                  className={`${
-                    active ? "bg-blue-200" : "text-white"
-                  } w-full font-semibold inline-flex items-center justify-center gap-x-3 text-amber-900 text-center py-4 `}
-                  href='#'>
-                  <BiMailSend
-                    className={`  text-blue-950 text-2xl font-semibold `}
-                  />
-                </a>
-              )}
-            </Menu.Item>
-          </div>
-        </Menu.Items>
-      </Transition>
-    </Menu>
+              <div className='flex justify-center divide-x divide-gray-400 bg-blue-100'>
+                <Menu.Item className='flex justify-center'>
+                  {({ active }) => (
+                    <a
+                      className={`${active ? "bg-blue-200" : "text-white"
+                        } w-full font-semibold inline-flex items-center justify-center gap-x-3 text-amber-900 text-center py-4 `}
+                      href='#'>
+                      <MdCall
+                        className={`  text-blue-950 text-2xl font-semibold `}
+                      />
+                    </a>
+                  )}
+                </Menu.Item>
+
+                <Menu.Item>
+                  {({ active }) => (
+                    <a
+                      className={`${active ? "bg-blue-200" : "text-white"
+                        } w-full font-semibold inline-flex items-center justify-center gap-x-3 text-amber-900 text-center py-4 `}
+                      href='#'>
+                      <BiMailSend
+                        className={`  text-blue-950 text-2xl font-semibold `}
+                      />
+                    </a>
+                  )}
+                </Menu.Item>
+              </div>
+            </Menu.Items>
+          </Transition>
+        </Menu>}
+    </>
   );
 };
 
